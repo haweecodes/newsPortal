e4a8f588b1ed5efdadf9407d3380df1e6b67e67f
Sajib Hawee <sajib.hawee@selise.ch>
2022-04-26 01:16:00 +0600

    feat: detail page added

---
+++ src/views/Detail.vue
@@ -0,0 +1,43 @@
+<template>
+  <article>
+    <v-btn @click="returnToHome" icon>
+      <v-icon>mdi-arrow-left</v-icon>
+    </v-btn>
+    <h1 class="text-center ma-5">{{ getHeadlineDetail.title }}</h1>
+    <p class="text-center ma-5">
+      <strong>Written By:{{ getHeadlineDetail.author }}</strong>
+    </p>
+    <p class="text-center ma-5">
+      <strong>Published At: {{ getHeadlineDetail.publishedAt | transformDate }}</strong>
+    </p>
+
+    <v-img
+      :alt="getHeadlineDetail.title"
+      aspect-ratio="3"
+      contain
+      :src="getHeadlineDetail.urlToImage"
+    ></v-img>
+
+    <p class="ma-5">{{ getHeadlineDetail.content }}</p>
+  </article>
+</template>
+
+<script>
+export default {
+  name: "Detail",
+  computed: {
+    getHeadlineDetail: function () {
+      return this.$store.state.headlineDetail;
+    },
+  },
+  methods: {
+    returnToHome() {
+      this.$router.push("/");
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
