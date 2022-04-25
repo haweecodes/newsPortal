5ab5da1c47c51e680f4b10ab466b329de5f0cdfa
Sajib Hawee <sajib.hawee@selise.ch>
2022-04-26 01:11:31 +0600

    feat: router added

---
+++ src/router/routes.js
@@ -0,0 +1,12 @@
+
+const routes = [{
+    path: '/',
+    component: () => import( /* webpackChunkName: "about" */ '../views/Home.vue')
+  },
+  {
+    path: '/detail',
+    component: () => import( /* webpackChunkName: "about" */ '../views/Detail.vue')
+  },
+]
+
+export default routes;
